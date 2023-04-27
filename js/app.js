af95e213bdfe25317c2e3d9b5f16de5f3953b8ce
Valentyn Slyvko <129826660+sitemasterReal@users.noreply.github.com>
2023-04-27 19:50:58 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,40 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    document.querySelector(".burger").addEventListener("click", (function() {
+        this.classList.toggle("active");
+        document.querySelector(".nav").classList.toggle("open");
+    }));
+    new Swiper(".swiper", {
+        speed: 1e3,
+        direction: "horizontal",
+        loop: true,
+        autoplay: {
+            delay: 4e3,
+            disableOnInteraction: false
+        }
+    });
+    window["FLS"] = true;
+    isWebp();
+})();
